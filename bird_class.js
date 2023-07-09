e1be6636a38089d0e51e231075256045218a8b46
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-09 16:22:04 +0900

    クラス構文

---
+++ bird_class.js
@@ -0,0 +1,37 @@
+class Bird {
+  #className = "鳥類";
+  constructor(name) {
+    this.name = name;
+  }
+
+  cry = (sound) => {
+    console.log(`${this.name}が「${sound}」と鳴きました`);
+  };
+
+  introduce = () => {
+    console.log(`私は${this.#className}の${this.name}です`);
+  };
+
+  static explain = () => {
+    console.log(`これは鳥のクラスです`);
+  };
+}
+
+// Birdを継承
+class FlyableBird extends Bird {
+  constructor(name) {
+    super(name);
+  }
+  fly = () => {
+    console.log(`${this.name}が飛びました`);
+  };
+}
+
+Bird.explain();
+
+const penguin = new Bird("ペンギン");
+penguin.introduce();
+
+const hawk = new FlyableBird("鷹");
+hawk.cry("ピィィー");
+hawk.fly();
