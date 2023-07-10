77f0e3e65976016ce76fd2ee158c8a96254a432b
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-10 23:35:01 +0900

    returnするだけなので命名する必要がない

--- greeter.js
+++ greeter.js
@@ -6,5 +6,15 @@ const greeter = (target) => {
   return sayHello;
 };
 
+// 関数型プログラミングでは不要な代入を避ける。上の関数はこう書き直せる
+const greeter2 = (target) => {
+  return () => {
+    console.log(`Hi, ${target}`);
+  };
+};
+
 const greet = greeter("Step Jun");
 greet();
+
+const greet2 = greeter2("hoge");
+greet2();
