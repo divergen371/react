e25121ebf2c4c0dd23cc2f4f439e12ea942f7e27
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-12 17:12:56 +0900

    commit

---
+++ typescript/index-signature.js
@@ -0,0 +1,9 @@
+"use strict";
+const myStatus = {
+    level: 22,
+    exp: 3058,
+    maxHP: 156,
+    maxMP: 174,
+    attack: 39,
+    defense: 25,
+};
---
+++ typescript/interface-sample.js
@@ -0,0 +1,4 @@
+"use strict";
+const tq = { rgb: "00afcc", opacity: 1 };
+tq.alias = "turquoise";
+tq.rgb = "03c1ff"; // 読み取り専用のプロパティに代入するとエラー
